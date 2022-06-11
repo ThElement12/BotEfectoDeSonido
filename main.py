39da2c9ed2ebc7eef788529078195fdcc0a6a8d7
Joseph De Los Santos <45013990+ThElement12@users.noreply.github.com>
2022-06-10 20:04:36 -0400

    Agregando el maldito sonido de logan

--- main.py
+++ main.py
@@ -95,6 +95,21 @@ async def desorden(ctx):
     else:
         await bot.send('User is not in a channel.')
 
+@bot.command(name="augh")
+async def augh(ctx):
+    voice_channel = ctx.author.voice.channel
+    if voice_channel is not None:
+        vc = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        if not vc or not vc.is_connected():
+            vc = await voice_channel.connect()
+        vc.play(discord.FFmpegPCMAudio('Sounds/Auuughhht.mp3'), after=lambda e: print('done', e))
+        while vc.is_playing():
+            await asyncio.sleep(1)
+        # disconnect after the player has finished
+        vc.stop()
+    else:
+        await bot.send('User is not in a channel.')
+
 
 @bot.command(name="babaji")
 async def babaji(ctx):
